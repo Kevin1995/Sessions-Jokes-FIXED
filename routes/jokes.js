f600a8b21df5224b20e1bcf9d2c9c439ea194376
Kevin1995 <kevinkehoe18@gmail.com>
2017-03-01 11:30:06 +0000

    Have the textarea logging out my joke now to make an array

--- routes/jokes.js
+++ routes/jokes.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('jokes', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  var jokesTyped = req.body.jokesTyped;
+
+  jokesTyped = jokesTyped.trim();
+
+  if(jokesTyped !== ""){
+    req.session.jokesTyped = jokesTyped;
+    console.log(jokesTyped);
+    res.redirect('/jokes');
+  }
+});
+
 module.exports = router;
